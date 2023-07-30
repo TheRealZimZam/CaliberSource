f9b0ba8c75786dab8ed9b5e3ec69abc2b4eb8d6e
_ZimZam <59622765+TheRealZimZam@users.noreply.github.com>
2023-07-30 02:48:42 -0400

    Add files via upload

---
+++ src/game/shared/basebludgeonweapon_shared.cpp
@@ -0,0 +1,416 @@
+//========= Copyright © 1996-2005, Valve Corporation, All rights reserved. ============//
+//
+// Purpose:		The class from which all bludgeon melee
+//				weapons are derived. 
+//
+// $Workfile:     $
+// $Date:         $
+// $NoKeywords: $
+//=============================================================================//
+
+#include "cbase.h"
+#include "basebludgeonweapon_shared.h"
+#include "baseplayer_shared.h"
+#include "gamerules.h"
+#include "ammodef.h"
+#include "mathlib/mathlib.h"
+#include "in_buttons.h"
+#include "animation.h"
+#include "rumble_shared.h"
+#include "GameStats.h"
+
+#if !defined( CLIENT_DLL )
+	#include "soundent.h"
+	#include "ai_condition.h"
+	#include "ndebugoverlay.h"
+	#include "te_effect_dispatch.h"
+	#include "ilagcompensationmanager.h"
+#endif
+
+// memdbgon must be the last include file in a .cpp file!!!
+#include "tier0/memdbgon.h"
+
+IMPLEMENT_NETWORKCLASS_ALIASED( BaseBludgeonWeapon, DT_BaseBludgeonWeapon )
+
+BEGIN_NETWORK_TABLE( CBaseBludgeonWeapon, DT_BaseBludgeonWeapon )
+END_NETWORK_TABLE()
+
+BEGIN_PREDICTION_DATA( CBaseBludgeonWeapon )
+END_PREDICTION_DATA()
+
+#define BLUDGEON_HULL_DIM		16
+
+static const Vector g_bludgeonMins(-BLUDGEON_HULL_DIM,-BLUDGEON_HULL_DIM,-BLUDGEON_HULL_DIM);
+static const Vector g_bludgeonMaxs(BLUDGEON_HULL_DIM,BLUDGEON_HULL_DIM,BLUDGEON_HULL_DIM);
+
+//-----------------------------------------------------------------------------
+// Constructor
+//-----------------------------------------------------------------------------
+CBaseBludgeonWeapon::CBaseBludgeonWeapon()
+{
+	m_bFiresUnderwater = true;
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: Spawn the weapon
+//-----------------------------------------------------------------------------
+void CBaseBludgeonWeapon::Spawn( void )
+{
+	m_fMinRange1	= 0;
+	m_fMinRange2	= 0;
+	m_fMaxRange1	= GetRange();
+	m_fMaxRange2	= GetRange();
+	//Call base class first
+	BaseClass::Spawn();
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: Precache the weapon
+//-----------------------------------------------------------------------------
+void CBaseBludgeonWeapon::Precache( void )
+{
+	//Call base class first
+	BaseClass::Precache();
+}
+
+int CBaseBludgeonWeapon::CapabilitiesGet()
+{ 
+#ifndef CLIENT_DLL
+	return bits_CAP_WEAPON_MELEE_ATTACK1;
+#else
+	return 0;
+#endif
+}
+
+int CBaseBludgeonWeapon::WeaponMeleeAttack1Condition( float flDot, float flDist )
+{
+#ifndef CLIENT_DLL
+	if (flDist > m_fMaxRange1)
+	{
+		return COND_TOO_FAR_TO_ATTACK;
+	}
+	else if (flDist < m_fMinRange1)
+	{
+		return COND_TOO_CLOSE_TO_ATTACK;
+	}
+	else if (flDot < 0.7)
+	{
+		return COND_NOT_FACING_ATTACK;
+	}
+
+	return COND_CAN_MELEE_ATTACK1;
+#else
+	return 0;
+#endif
+}
+
+//------------------------------------------------------------------------------
+// Purpose : Update weapon
+//------------------------------------------------------------------------------
+void CBaseBludgeonWeapon::ItemPostFrame( void )
+{
+	CBasePlayer *pOwner = ToBasePlayer( GetOwner() );
+	
+	if ( pOwner == NULL )
+		return;
+
+	if ( (pOwner->m_nButtons & IN_ATTACK) && (m_flNextPrimaryAttack <= gpGlobals->curtime) )
+	{
+		PrimaryAttack();
+	} 
+	else if ( (pOwner->m_nButtons & IN_ATTACK2) && (m_flNextSecondaryAttack <= gpGlobals->curtime) )
+	{
+		SecondaryAttack();
+	}
+	else 
+	{
+		WeaponIdle();
+		return;
+	}
+}
+
+//------------------------------------------------------------------------------
+// Purpose :
+// Input   :
+// Output  :
+//------------------------------------------------------------------------------
+void CBaseBludgeonWeapon::PrimaryAttack()
+{
+#ifndef CLIENT_DLL
+	CBasePlayer *pPlayer = ToBasePlayer( GetOwner() );
+	// Move other players back to history positions based on local player's lag
+	lagcompensation->StartLagCompensation( pPlayer, pPlayer->GetCurrentCommand() );
+#endif
+
+	Swing( false );
+
+#ifndef CLIENT_DLL
+	// Move other players back to history positions based on local player's lag
+	lagcompensation->FinishLagCompensation( pPlayer );
+#endif
+}
+
+void CBaseBludgeonWeapon::SecondaryAttack()
+{
+	Swing( true );
+}
+
+
+//------------------------------------------------------------------------------
+// Purpose: Implement impact function
+//------------------------------------------------------------------------------
+void CBaseBludgeonWeapon::Hit( trace_t &traceHit, Activity nHitActivity, bool bIsSecondary )
+{
+	CBasePlayer *pPlayer = ToBasePlayer( GetOwner() );
+	
+	//Do view kick
+	AddViewKick();
+
+#ifndef CLIENT_DLL
+	//Make sound for the AI
+	CSoundEnt::InsertSound( SOUND_BULLET_IMPACT, traceHit.endpos, 300, 0.2f, pPlayer );
+	// This isn't great, but it's something for when the crowbar hits.
+	pPlayer->RumbleEffect( RUMBLE_AR2, 0, RUMBLE_FLAG_RESTART );
+#endif
+
+	CBaseEntity	*pHitEntity = traceHit.m_pEnt;
+
+	//Apply damage to a hit target
+	if ( pHitEntity != NULL )
+	{
+		Vector hitDirection;
+		pPlayer->EyeVectors( &hitDirection, NULL, NULL );
+		VectorNormalize( hitDirection );
+
+#ifndef CLIENT_DLL
+		CTakeDamageInfo info( GetOwner(), GetOwner(), GetDamageForActivity( nHitActivity ), GetDamageType() );
+
+		if( pPlayer && pHitEntity->IsNPC() )
+		{
+			// If bonking an NPC, adjust damage.
+			info.AdjustPlayerDamageInflictedForSkillLevel();
+		}
+
+		CalculateMeleeDamageForce( &info, hitDirection, traceHit.endpos );
+
+		pHitEntity->DispatchTraceAttack( info, hitDirection, &traceHit ); 
+		ApplyMultiDamage();
+
+		// Now hit all triggers along the ray that... 
+		TraceAttackToTriggers( info, traceHit.startpos, traceHit.endpos, hitDirection );
+
+		if ( ToBaseCombatCharacter( pHitEntity ) )
+			gamestats->Event_WeaponHit( pPlayer, !bIsSecondary, GetClassname(), info );
+#endif
+	}
+
+	// Apply an impact effect
+	ImpactEffect( traceHit );
+}
+
+Activity CBaseBludgeonWeapon::ChooseIntersectionPointAndActivity( trace_t &hitTrace, const Vector &mins, const Vector &maxs, CBasePlayer *pOwner )
+{
+	int			i, j, k;
+	float		distance;
+	const float	*minmaxs[2] = {mins.Base(), maxs.Base()};
+	trace_t		tmpTrace;
+	Vector		vecHullEnd = hitTrace.endpos;
+	Vector		vecEnd;
+
+	distance = 1e6f;
+	Vector vecSrc = hitTrace.startpos;
+
+	vecHullEnd = vecSrc + ((vecHullEnd - vecSrc)*2);
+	UTIL_TraceLine( vecSrc, vecHullEnd, MASK_SHOT_HULL, pOwner, COLLISION_GROUP_NONE, &tmpTrace );
+	if ( tmpTrace.fraction == 1.0 )
+	{
+		for ( i = 0; i < 2; i++ )
+		{
+			for ( j = 0; j < 2; j++ )
+			{
+				for ( k = 0; k < 2; k++ )
+				{
+					vecEnd.x = vecHullEnd.x + minmaxs[i][0];
+					vecEnd.y = vecHullEnd.y + minmaxs[j][1];
+					vecEnd.z = vecHullEnd.z + minmaxs[k][2];
+
+					UTIL_TraceLine( vecSrc, vecEnd, MASK_SHOT_HULL, pOwner, COLLISION_GROUP_NONE, &tmpTrace );
+					if ( tmpTrace.fraction < 1.0 )
+					{
+						float thisDistance = (tmpTrace.endpos - vecSrc).Length();
+						if ( thisDistance < distance )
+						{
+							hitTrace = tmpTrace;
+							distance = thisDistance;
+						}
+					}
+				}
+			}
+		}
+	}
+	else
+	{
+		hitTrace = tmpTrace;
+	}
+
+
+	return ACT_VM_HITCENTER;
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+// Input  : &traceHit - 
+//-----------------------------------------------------------------------------
+bool CBaseBludgeonWeapon::ImpactWater( const Vector &start, const Vector &end )
+{
+	//FIXME: This doesn't handle the case of trying to splash while being underwater, but that's not going to look good
+	//		 right now anyway...
+
+	// We must start outside the water
+	if ( UTIL_PointContents( start ) & (CONTENTS_WATER|CONTENTS_SLIME))
+		return false;
+
+	// We must end inside of water
+	if ( !(UTIL_PointContents( end ) & (CONTENTS_WATER|CONTENTS_SLIME)))
+		return false;
+
+	trace_t	waterTrace;
+
+	UTIL_TraceLine( start, end, (CONTENTS_WATER|CONTENTS_SLIME), GetOwner(), COLLISION_GROUP_NONE, &waterTrace );
+
+	if ( waterTrace.fraction < 1.0f )
+	{
+#ifndef CLIENT_DLL
+		CEffectData	data;
+
+		data.m_fFlags  = 0;
+		data.m_vOrigin = waterTrace.endpos;
+		data.m_vNormal = waterTrace.plane.normal;
+		data.m_flScale = 8.0f;
+
+		// See if we hit slime
+		if ( waterTrace.contents & CONTENTS_SLIME )
+		{
+			data.m_fFlags |= FX_WATER_IN_SLIME;
+		}
+
+		DispatchEffect( "watersplash", data );
+#endif		
+	}
+
+	return true;
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+void CBaseBludgeonWeapon::ImpactEffect( trace_t &traceHit )
+{
+	// See if we hit water (we don't do the other impact effects in this case)
+	if ( ImpactWater( traceHit.startpos, traceHit.endpos ) )
+		return;
+
+	//FIXME: need new decals, currently does none as stopgap
+	UTIL_ImpactTrace( &traceHit, GetDamageType(), "MeleeImpact" );
+}
+
+
+//------------------------------------------------------------------------------
+// Purpose : Starts the swing of the weapon and determines the animation
+// Input   : bIsSecondary - is this a secondary attack?
+//------------------------------------------------------------------------------
+void CBaseBludgeonWeapon::Swing( int bIsSecondary )
+{
+	trace_t traceHit;
+
+	// Try a ray
+	CBasePlayer *pOwner = ToBasePlayer( GetOwner() );
+	if ( !pOwner )
+		return;
+
+#ifndef CLIENT_DLL
+	pOwner->RumbleEffect( RUMBLE_CROWBAR_SWING, 0, RUMBLE_FLAG_RESTART );
+#endif
+	Vector swingStart = pOwner->Weapon_ShootPosition( );
+	Vector forward;
+
+	forward = pOwner->GetAutoaimVector( AUTOAIM_SCALE_DEFAULT );
+	//pOwner->EyeVectors( &forward, NULL, NULL );
+
+	Vector swingEnd = swingStart + forward * GetRange();
+	UTIL_TraceLine( swingStart, swingEnd, MASK_SHOT_HULL, pOwner, COLLISION_GROUP_NONE, &traceHit );
+	Activity nHitActivity = ACT_VM_HITCENTER;
+
+#ifndef CLIENT_DLL
+	// Like bullets, bludgeon traces have to trace against triggers.
+	CTakeDamageInfo triggerInfo( GetOwner(), GetOwner(), GetDamageForActivity( nHitActivity ), GetDamageType() );
+	triggerInfo.SetDamagePosition( traceHit.startpos );
+	triggerInfo.SetDamageForce( forward );
+	TraceAttackToTriggers( triggerInfo, traceHit.startpos, traceHit.endpos, forward );
+#endif
+
+	if ( traceHit.fraction == 1.0 )
+	{
+		float bludgeonHullRadius = 1.732f * BLUDGEON_HULL_DIM;  // hull is +/- 16, so use cuberoot of 2 to determine how big the hull is from center to the corner point
+
+		// Back off by hull "radius"
+		swingEnd -= forward * bludgeonHullRadius;
+
+		UTIL_TraceHull( swingStart, swingEnd, g_bludgeonMins, g_bludgeonMaxs, MASK_SHOT_HULL, pOwner, COLLISION_GROUP_NONE, &traceHit );
+		if ( traceHit.fraction < 1.0 && traceHit.m_pEnt )
+		{
+			Vector vecToTarget = traceHit.m_pEnt->GetAbsOrigin() - swingStart;
+			VectorNormalize( vecToTarget );
+
+			float dot = vecToTarget.Dot( forward );
+
+			// YWB:  Make sure they are sort of facing the guy at least...
+			if ( dot < 0.70721f )
+			{
+				// Force amiss
+				traceHit.fraction = 1.0f;
+			}
+			else
+			{
+				nHitActivity = ChooseIntersectionPointAndActivity( traceHit, g_bludgeonMins, g_bludgeonMaxs, pOwner );
+			}
+		}
+	}
+
+#ifndef CLIENT_DLL
+//	if ( !bIsSecondary )
+//		m_iPrimaryAttacks++;
+//	else 
+//		m_iSecondaryAttacks++;
+
+	gamestats->Event_WeaponFired( pOwner, !bIsSecondary, GetClassname() );
+#endif
+
+	// -------------------------
+	//	Miss
+	// -------------------------
+	if ( traceHit.fraction == 1.0f )
+	{
+		nHitActivity = bIsSecondary ? ACT_VM_MISSCENTER2 : ACT_VM_MISSCENTER;
+
+		// We want to test the first swing again
+		Vector testEnd = swingStart + forward * GetRange();
+		
+		// See if we happened to hit water
+		ImpactWater( swingStart, testEnd );
+	}
+	else
+	{
+		Hit( traceHit, nHitActivity, bIsSecondary ? true : false );
+	}
+
+	// Send the anim
+	SendWeaponAnim( nHitActivity );
+
+	//Setup our next attack times
+	m_flNextPrimaryAttack = gpGlobals->curtime + GetFireRate();
+	m_flNextSecondaryAttack = gpGlobals->curtime + SequenceDuration();
+
+	//Play swing sound
+	WeaponSound( SINGLE );
+}
---
+++ src/game/shared/basebludgeonweapon_shared.h
@@ -0,0 +1,69 @@
+//========= Copyright © 1996-2005, Valve Corporation, All rights reserved. ============//
+//
+// Purpose:		The class from which all bludgeon melee
+//				weapons are derived. 
+//
+// $Workfile:     $
+// $Date:         $
+// $NoKeywords: $
+//=============================================================================//
+
+#include "cbase.h"
+#include "basecombatweapon_shared.h"
+
+#ifndef BASEBLUDGEONWEAPON_H
+#define BASEBLUDGEONWEAPON_H
+
+#ifdef _WIN32
+#pragma once
+#endif
+
+#if defined( CLIENT_DLL )
+#define CBaseBludgeonWeapon C_BaseBludgeonWeapon
+#endif
+
+//=========================================================
+// CBaseBludgeonWeapon 
+//=========================================================
+class CBaseBludgeonWeapon : public CBaseCombatWeapon
+{
+	DECLARE_CLASS( CBaseBludgeonWeapon, CBaseCombatWeapon );
+public:
+	CBaseBludgeonWeapon();
+
+	DECLARE_NETWORKCLASS(); 
+	DECLARE_PREDICTABLE();
+	virtual bool	IsPredicted() const { return true; }
+
+	virtual	void	Spawn( void );
+	virtual	void	Precache( void );
+
+	//Attack functions
+	virtual	void	PrimaryAttack( void );
+	virtual	void	SecondaryAttack( void );
+	
+	virtual void	ItemPostFrame( void );
+
+	//Functions to select animation sequences 
+	virtual Activity	GetPrimaryAttackActivity( void )	{	return	ACT_VM_HITCENTER;	}
+	virtual Activity	GetSecondaryAttackActivity( void )	{	return	ACT_VM_HITCENTER2;	}
+
+	virtual	float	GetFireRate( void )								{	return	0.2f;	}
+	virtual float	GetRange( void )								{	return	32.0f;	}
+	virtual	int		GetDamageType( void )							{	return	DMG_CLUB;	}
+	virtual	float	GetDamageForActivity( Activity hitActivity )	{	return	1.0f;	}
+
+	virtual int		CapabilitiesGet( void );
+	virtual	int		WeaponMeleeAttack1Condition( float flDot, float flDist );
+
+protected:
+	virtual	void	ImpactEffect( trace_t &trace );
+
+private:
+	bool			ImpactWater( const Vector &start, const Vector &end );
+	void			Swing( int bIsSecondary );
+	void			Hit( trace_t &traceHit, Activity nHitActivity, bool bIsSecondary );
+	Activity		ChooseIntersectionPointAndActivity( trace_t &hitTrace, const Vector &mins, const Vector &maxs, CBasePlayer *pOwner );
+};
+
+#endif
